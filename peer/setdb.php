51c81b45bc21f1fc988fa590e8b6876ca4a9d20d
Pieter van den Hombergh <p.vandenhombergh@fontys.nl>
2015-08-03 16:05:00 +0200

    simplified configuration

--- peer/setdb.php
+++ peer/setdb.php
@@ -5,11 +5,11 @@
  * @package peerweb
  * $Id: setdb.php,v 1.15 2009/01/03 08:01:49 hom Exp $
  */
-$db_name = 'peer';
-$site_userdir='/home/f/fontysvenlo.org/';
+$db_name = 'peer2';
+$site_userdir='/home/hom/';
 $site_home=$site_userdir.'peerweb/';
 $server_url='https://www.fontysvenlo.org';
 define('ADMIN_EMAILADDRESS','p.vandenhombergh@fontys.nl');
-$root_url='/peerweb'; // as seen from the browser
+$root_url='/peertest'; // as seen from the browser
 require_once 'configure.php';
 
