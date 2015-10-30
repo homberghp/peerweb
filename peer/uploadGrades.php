6e44b2dda2d0aa2c494e07a488e3ca3eb4ec8cc2
pieter <p.vandenhombergh@fontys.nl>
2015-10-30 12:55:14 +0100

    upload grades working

--- peer/uploadGrades.php
+++ peer/uploadGrades.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once 'component.inc';
 require_once 'navigation2.inc';
 require_once 'GradeHarvester.php';
@@ -8,8 +9,11 @@ require_once 'GradeHarvester.php';
 $gradeSet = array();
 $showInput = true;
 $event = '';
-if (isSet($_POST['grades']) && isSet($_POST['event'])) {
+if (isSet($_POST['event'])) {
     $_SESSION['event'] = $event = pg_escape_string($_POST['event']);
+}
+
+if (isSet($_POST['grades']) && isSet($_POST['event'])) {
     $_SESSION['gradeSet'] = $gradeSet = harvest($_POST['grades']);
     $showInput = (count($gradeSet) === 0);
 } else if (isSet($_POST['grade']) && isSet($_POST['cand']) && isSet($_POST['commit'])) {
@@ -19,13 +23,16 @@ if (isSet($_POST['grades']) && isSet($_POST['event'])) {
         $gradeSet[$_POST['cand'][$c]] = $_POST['grade'][$c];
     }
     $cands = implode(',', array_keys($gradeSet));
-    $trans_id = $dbConn->transactionStart('enter grades for ' . $_POST['event']);
-    $sql = "delete from exam_grades where event = '$event' and snummer "
-            . "in ($cand) and trans_id in (select trans_id from transaction where operator=$peer_id);\n";
+    $trans_id = $dbConn->transactionStart("enter grades for  {$event}");
+    $sql = "delete from exam_grades where event = '{$event}' and snummer "
+            . "in ($cands) and trans_id in (select trans_id from transaction where operator=$peer_id);\n";
+    $resultSet = $dbConn->Execute($sql);
     foreach ($gradeSet as $can => $grad) {
-        $sql .="insert into exam_grades (snummer,event,grade,trans_id) values ($can,'$event',$grad,$trans_id;\n";
+        $sql = "insert into exam_grades (snummer,event,grade,trans_id) values ({$can},'{$event}',{$grad},{$trans_id});\n";
+        $resultSet = $dbConn->Execute($sql);
     }
-    echo "<pre>{$sql}</pre>\n";
+    //echo "<pre>{$sql}</pre>\n";
+    $resultSet = $dbConn->Execute($sql);
     $dbConn->transactionEnd();
 
     unSet($_SESSION['gradeSet']);
@@ -34,7 +41,8 @@ $rows = "";
 if (count($gradeSet)) {
 
     $cands = implode(',', array_keys($gradeSet));
-    $sql = "select snummer, achternaam,roepnaam,coalesce(', '||voorvoegsel,'') as voorvoegsel from student where snummer in ($cands) order by achternaam,roepnaam";
+    $sql = "select snummer, achternaam,roepnaam,coalesce(', '||voorvoegsel,'') as voorvoegsel "
+            . "from student where snummer in ({$cands}) order by achternaam,roepnaam";
     $resultSet = $dbConn->Execute($sql);
 
     if ($resultSet === FALSE) {
