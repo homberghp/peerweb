2f7a199f03984c90d0034ca45e32b7ad3e19d721
Pieter van den Hombergh <p.vandenhombergh@fontys.nl>
2016-01-08 16:32:35 +0100

    simpler is better

--- views/try_close.sql
+++ views/try_close.sql
@@ -10,18 +10,15 @@ BEGIN
    -- close for this judge
    UPDATE prj_grp SET written=true,prj_grp_open=false WHERE prjtg_id=gid AND snummer=stid;
    SELECT bool_and(written) AS allwritten,
-          bool_or(prj_grp_open) AS any_open, 
-          prj_tutor.prj_tutor_open, prj_tutor.assessment_complete
-   INTO allwritten,any_open,prj_tutor_open,assessment_complete
+          bool_or(prj_grp_open) AS any_open
+   INTO allwritten,any_open
    FROM public.prj_grp 
    JOIN prj_tutor USING(prjtg_id)
    WHERE prjtg_id=gid 
-   GROUP by prjtg_id,prj_tutor.prj_tutor_open,prj_tutor.assessment_complete;
+   GROUP by prjtg_id,prj_tutor.prj_tutor_open;
    
    -- if nothing to be done, return false
-   IF (NOT allwritten OR any_open) 
-      AND NOT prj_tutor_open 
-      OR assessment_complete THEN
+   IF (NOT allwritten OR any_open) THEN
    -- nothing to do
       RETURN FALSE; 
    ELSE 
