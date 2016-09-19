4430ab9b5061450f37e665d4a70295c4e069938a
Pieter van den Hombergh <p.vandenhombergh@fontys.nl>
2016-09-19 11:48:32 +0200

    link

--- peer/grouplist.php
+++ peer/grouplist.php
@@ -52,7 +52,7 @@ $spreadSheetWriter->setFilename($filename)
 $spreadSheetWriter->processRequest();
 
 $spreadSheetWidget = $spreadSheetWriter->getWidget();
-$sqlhead2 = "select distinct '<a href=\"student_admin.php?snummer='||s.snummer||'\">'||s.snummer||'</a>' as snummer,";
+$sqlhead = "select distinct '<a href=\"student_admin.php?snummer='||s.snummer||'\" target=\"_blank\">'||s.snummer||'</a>' as snummer,";
 
 
 $rainbow = new RainBow(STARTCOLOR, COLORINCREMENT_RED, COLORINCREMENT_GREEN, COLORINCREMENT_BLUE);
