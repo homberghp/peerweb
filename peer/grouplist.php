e8247f9a0eadd4d8e3efbf5c0e42b7bfd7662cbc
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2016-09-21 17:17:01 +0200

    dropped sqlhead2

--- peer/grouplist.php
+++ peer/grouplist.php
@@ -77,7 +77,7 @@ $prjSel->setJoin('milestone_grp using (prj_id,milestone)');
 $prj_id_selector = $prjSel->getSelector();
 $emailList = '';
 $grpList = array();
-$resultSet = $dbConn->Execute($sqlhead2 . $sqltail);
+$resultSet = $dbConn->Execute($sqlhead . $sqltail);
 if ($resultSet === false) {
     $dbConn->log('failed wth' . $dbConn->ErrorMsg());
 }
