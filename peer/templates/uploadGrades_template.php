07ff5ef7a48df9d76f271890243b43a3b6164297
pieter <p.vandenhombergh@fontys.nl>
2017-06-03 14:23:48 +0200

    upload grades

---
+++ peer/templates/uploadGrades_template.php
@@ -0,0 +1,20 @@
+<fieldset><legend>Grade Uploader</legend>
+    <?php if ($showInput) { ?>
+        <form name='upload' method="post" action="<?= $PHP_SELF ?>">
+            <p>Choose and <b>event code</b>, typically progress-code -date and then copy and paste your set of grades into the window below.<br/>
+                Note that any grade from the same (<b>student, exam event</b>) will overwrite previous grades.</p><br/>
+            <label for='event'>Event code</label><?= $event_selector ?><br/>
+            <textarea name='grades' cols="80" rows="40"></textarea>
+            <br/>
+            <input type='submit' name='submit' value='submit'/>
+        </form>
+    <?php } else { ?>
+        <h2>grades found for event <b><?= $event_code ?></b></h2>
+        <form name='upload' method="post" action="<?= $PHP_SELF ?>">
+
+            <?= $rows ?>
+            <input type='hidden' name='exam_event' value='<?= $exam_event ?>'/>
+            <input type='submit' name='commit' value='commit'/>
+
+        </form>
+    <?php } ?>
