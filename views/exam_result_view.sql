c20e96643c664e68f2c7e7e840d43afe3d9e5e36
Pieter van den Hombergh <p.vandenhombergh@fontys.nl>
2017-06-06 10:47:56 +0200

    added result_view

---
+++ views/exam_result_view.sql
@@ -0,0 +1,14 @@
+begin work;
+
+drop view if exists exam_result_view;
+create view exam_result_view as
+       select snummer,achternaam,roepnaam, cohort,
+       exam_date,module_part.progress_code,
+       grade
+       from exam_grades join exam_event using (exam_event_id)
+       join module_part using(module_part_id)
+       join student using(snummer)
+       ;
+
+commit;
+       
