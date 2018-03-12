f2e7b9970f5c7ecf63a223c02fdb6bcdf010a6d2
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2018-03-12 16:06:38 +0100

    grp details repaired

--- peer/grpdetails.php
+++ peer/grpdetails.php
@@ -15,38 +15,40 @@ function getOrNull($arr, $g, $n) {
     if (isSet($arr[$n][$g]) && ($arr[$n][$g] !== '')) {
         return '\'' . pg_escape_string($arr[$n][$g]) . '\'';
     } else {
-        return 'null';
+        return null;
     }
 }
 
 if (isSet($_POST['submit'])) {
-    $sql = "begin work;\n"
-            . "delete from grp_alias where prjtg_id in (select prjtg_id from prj_tutor where prjm_id={$prjm_id});\n";
-    $grps = count($_POST['prjtg_id']);
-    /* echo "<pre>"; */
-    /* print_r($_POST); */
-    /* echo "</pre>"; */
 
-    for ($g = 0; $g < $grps; $g++) {
-        $long_name = getOrNull($_POST, $g, 'long_name');
-        $alias = getOrNull($_POST, $g, 'alias');
-        $website = getOrNull($_POST, $g, 'website');
-        $productname = getOrNull($_POST, $g, 'productname');
-        $prjtg_id = getOrNull($_POST, $g, 'prjtg_id');
-        $youtube_link = getOrNull($_POST, $g, 'youtube_link');
-        $youtube_icon_url = getOrNull($_POST, $g, 'youtube_icon_url');
-        if ($prjtg_id != 'null') {
-            $sql .="insert into grp_alias (prjtg_id,alias,long_name,website,productname,youtube_link,youtube_icon_url)\n"
-                    . "\nvalues ($prjtg_id,$alias,$long_name,$website,$productname,$youtube_link,$youtube_icon_url);\n";
+    $grps = count($_POST['prjtg_id']);
+    if ($grps > 0) {
+        $sql = <<<'SQL'
+insert into grp_alias (prjtg_id,alias,long_name,website,productname,youtube_link,youtube_icon_url)
+    values($1,$2,$3,$4,$5,$6,$7);
+SQL;
+        $dbConn->Execute("begin work;");
+        try {
+            $statement = $dbConn->Prepare($sql);
+
+            for ($g = 0; $g < $grps; $g++) {
+                $long_name = $_POST['long_name'][$g];
+                $alias = $_POST['alias'][$g];
+                $website = $_POST['website'][$g];
+                $productname = $_POST['productname'][$g];
+                $prjtg_id = $_POST['prjtg_id'][$g];
+                $youtube_link = $_POST['youtube_link'][$g];
+                $youtube_icon_url = $_POST['youtube_icon_url'][$g];
+                
+                $statement->execute([$prjtg_id, $alias, $long_name, $website, $productname, $youtube_link, $youtube_icon_url]);
+            }
+            $dbConn->Execute("commit;");
+        } catch (Exception $se) {
+            $dbConn->Execute("rollback;");
         }
     }
-    $sql .= "commit;\n";
-//    "<pre>$sql</pre>";
-    $resultSet = $dbConn->Execute($sql);
-    if ($resultSet === false) {
-        die("<br>Cannot set grop details with <pre>" . $sql . "</pre> reason " . $dbConn->ErrorMsg() . "<br>");
-    }
 }
+
 $prjSel = new PrjMilestoneSelector2($dbConn, $peer_id, $prjm_id);
 $pSel = $prjSel->getWidget();
 extract($prjSel->getSelectedData());
@@ -54,15 +56,19 @@ $_SESSION['prj_id'] = $prj_id;
 $_SESSION['prjm_id'] = $prjm_id;
 $_SESSION['milestone'] = $milestone;
 $doctype_set = array();
-$sql = "select pt.grp_num,ga.* from prj_tutor pt left join grp_alias ga using(prjtg_id) where prjm_id=$prjm_id order by grp_num";
+$sql = "select pt.grp_num, pt.prjtg_id, ga.long_name, ga.alias, ga.website, ga.productname,\n"
+        . "ga.youtube_link ,ga.youtube_icon_url\n".
+        " from prj_tutor pt left join grp_alias ga using(prjtg_id) where prjm_id=$prjm_id order by grp_num";
+//echo "<pre>$sql</pre>";
 $inputColumns = array(
-    '1' => array('type' => 'T', 'size' => '40'),
-    '2' => array('type' => 'T', 'size' => '15'),
-    '3' => array('type' => 'T', 'size' => '64'),
-    '4' => array('type' => 'N', 'size' => '64'),
-    '5' => array('type' => 'H', 'size' => '0'),
-    '6' => array('type' => 'T', 'size' => '64'),
-    '7' => array('type' => 'T', 'size' => '64'),
+//    '0' => array('type' => 'N', 'size' => '6'), // grpnum
+    '1' => array('type' => 'H', 'size' => '6'), // prjtg_id
+    '2' => array('type' => 'T', 'size' => '15'), // long_name
+    '3' => array('type' => 'T', 'size' => '64'), // alias
+    '4' => array('type' => 'T', 'size' => '64'), // website
+    '5' => array('type' => 'T', 'size' => '64'), // product_name
+    '6' => array('type' => 'T', 'size' => '64'), // yt link
+    '7' => array('type' => 'T', 'size' => '64'), // yt_url
 );
 $table = getQueryToTableChecked2($dbConn, $sql, false, -1, new RainBow(0x46B4B4, 64, 32, 0), 'document[]', $doctype_set, $inputColumns);
 
@@ -84,9 +90,9 @@ $nav = new Navigation(array(), basename($PHP_SELF), $page_opening);
 $nav->show();
 ?>
 <div id='navmain' style='padding:1em;'>
-    <?= $pSel ?>
+<?= $pSel ?>
     <form method="post" action="<?= $PHP_SELF; ?>" >
-        <?= $table ?>
+    <?= $table ?>
         <input type='reset' name='reset' value='Reset Form'/>
         <input type='submit' name='submit' value='Submit Form'/>
     </form>
