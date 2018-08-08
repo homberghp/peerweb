df1294542bbf715bcf12d6dec922e54174bfe07d
Pieter van den Hombergh <p.vandenhombergh@fontys.nl>
2018-08-08 10:46:29 +0200

    switchers and classcard for 2018

--- jmerge/sql/sv05_switchers.sql
+++ jmerge/sql/sv05_switchers.sql
@@ -1,1 +1,1 @@
-select p.* from prospects p join student s on (s.snummer=p.snummer and s.opl<> p.opl) order by hoofdgrp,achternaam,roepnaam;
+select p.*,s.hoofdgrp as fromgroup from prospects p join student s on (s.snummer=p.snummer and s.opl<> p.opl) order by hoofdgrp,achternaam,roepnaam;
