94c15d56052cae274172cb8d2f11a674a592f2ae
pieter <p.vandenhombergh@fontys.nl>
2018-10-19 12:51:50 +0200

    link to firefox

--- peer/peertree.php
+++ peer/peertree.php
@@ -48,7 +48,7 @@ require_once'treebuilder.php';
                                                       title='Make your own tablecard' 
                                                       style='margin: 4px 0 4px 0;' alt='Make your own tablecard' src='images/pietje.png'/></a>
       <!--Some 'commercials'-->
-      <a href='https://www.mozilla.org/products/firefox/' target='mainframe'>
+      <a href='https://www.mozilla.org' target='mainframe'>
         <img src='<?= IMAGEROOT ?>/product-firefox.png' border='0'
              alt='best viewed with mozilla or mozilla-firefox'
              title='best viewed with mozilla firefox'/>
