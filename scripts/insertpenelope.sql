a559d12d23c03576606034c419884c14fbfe0e49
pieter <p.vandenhombergh@fontys.nl>
2018-11-15 12:56:21 +0100

    added insert script for penelope

---
+++ scripts/insertpenelope.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO public.student VALUES (2882758, 'Puk', NULL, 'P', 'Penelope', NULL, NULL, NULL, NULL, 'l.urselmans@student.fontys.nl', 'DE', 1989, '1989-09-04', 'F', 'EN', 182758, 112, NULL, NULL, NULL, 47, 'TUTORINF', false, 877516, NULL, 864, 'Kleve', 'DEU', 'Linda', 411);
+
+
