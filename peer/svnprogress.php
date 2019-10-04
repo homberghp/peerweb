131b37f964d9519331846ad222635b3f8f79ec07
Pieter van den Hombergh <p.vandenhombergh@fontys.nl>
2019-10-04 16:53:59 +0200

    some small tweak, svn progress show cohort

--- peer/svnprogress.php
+++ peer/svnprogress.php
@@ -25,7 +25,7 @@ $filename = 'svnProgress_' . $afko . '-' . date('Ymd');
 $title = "Student and groups in project $afko milestone $milestone";
 $sql = "select '<a href=''student_admin.php?snummer='||snummer||''' target=''_blank''>'||snummer||'</a>' as snummer,"
      . "'<a href=''student_admin.php?snummer='||snummer||''' target=''_blank''><img src='''||photo||''' style=''height:32px;width:auto;''/></a>' as foto,"
-     ."achternaam,roepnaam,grp_name,"
+     ."achternaam,roepnaam,cohort,grp_name,"
      ."'<a href={$svnserver_url}'||url_tail||' target=_blank>'||grp_name||'</a>' as url,"
      ."youngest,last_commit from svn_progress join portrait using(snummer) where prjm_id={$prjm_id}";
 $spreadSheetWriter = new SpreadSheetWriter($dbConn, $sql);
