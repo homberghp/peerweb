5ee5137cc0c4a98dca77c86e3606e8bced8a1c8f
Pieter van den Hombergh <p.vandenhombergh@fontys.nl>
2019-12-19 10:15:45 +0100

    restored lang

--- peer/examlist.php
+++ peer/examlist.php
@@ -22,7 +22,7 @@ $_SESSION[ 'milestone' ] = $milestone;
 
 $filename = 'examlist_' . $afko . '-' . date( 'Ymd' );
 $sql = "select distinct snummer as id,achternaam as surname,roepnaam||coalesce(' '||tussenvoegsel,'') as name,trim(sclass) as sclass,voorletters\n"
-        . " ,'EN' as lang,prjm_id,trim(coalesce(alias,'g'||grp_num)) as alias, md5(prjm_id::text||prj_grp.snummer::text || now()) AS token, \n"
+        . " ,lang,prjm_id,trim(coalesce(alias,'g'||grp_num)) as alias, md5(prjm_id::text||prj_grp.snummer::text || now()) AS token, \n"
         . " cohort,email1,course_short as education, case when prjm_id=422 then 1 else 0 end as lo"
         . "  from student_email s join prj_grp using(snummer) \n"
         . " join all_prj_tutor using(prjtg_id)\n"
