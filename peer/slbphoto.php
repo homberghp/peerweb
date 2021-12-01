4ac17e63dc7b5ef1c86067461d250fd29bae0bca
Pieter van den Hombergh (homberghp) <p.vandenhombergh@fontys.nl>
2021-12-01 14:13:22 +0100

    intermediate commit

--- peer/slbphoto.php
+++ peer/slbphoto.php
@@ -1,49 +1,49 @@
 <?php
-requireCap(CAP_TUTOR);
+requireCap( CAP_TUTOR );
 require_once('validators.php');
 require_once('navigation2.php');
 require_once 'ClassSelectorClass.php';
 
-define('MAXROW', '4');
-define('MAXCOL', '5');
+define( 'MAXROW', '4' );
+define( 'MAXCOL', '5' );
 $class_id = '1';
 $tutor = $tutor_code;
 $slb = $peer_id;
-if (isSet($_REQUEST['slb'])) {
-    $_SESSION['slb'] = $hoofdgrp = validate($_REQUEST['slb'], 'integer', '0');
+if ( isSet( $_REQUEST[ 'slb' ] ) ) {
+    $_SESSION[ 'slb' ] = $hoofdgrp = validate( $_REQUEST[ 'slb' ], 'integer', '0' );
 }
 
-extract($_SESSION);
+extract( $_SESSION );
 $slb_info = "";
 $slb_sql = "select roepnaam||coalesce(' '||tussenvoegsel||' ',' ')||achternaam||' ('||tutor||':'||userid||')' as slb_info\n"
-        . " from tutor join student_email  s on (s.snummer=tutor.userid) where userid=$slb";
-$resultSet = $dbConn->Execute($slb_sql);
+        . " from tutor join student_email  s on (s.snummer=tutor.userid) where userid=?";
+$pst = $dbConn->prepare( $slb_sql );
+
 //echo "<pre>{$slb_sql}</pre>";
-if (!$resultSet->EOF) {
-    extract($resultSet->fields);
+if ( $pst->execute( [ $slb ] ) !== false ) {
+    extract( $pst->fetch() );
 }
-$year = date('Y');
+$year = date( 'Y' );
 # get actual course_year
 $sql = "select value as year from peer_settings where key='course_year'";
-$resultSet = $dbConn->Execute($sql);
-if (!$resultSet->EOF)
-    extract($resultSet->fields);
+$pstm = $dbConn->query( $sql );
+if ( $pstm != false )
+    extract( $pstm->fetch() );
 
-if (isSet($_REQUEST['class_id'])) {
-    $_SESSION['class_id'] = $class_id = $_REQUEST['class_id'];
+if ( isSet( $_REQUEST[ 'class_id' ] ) ) {
+    $_SESSION[ 'class_id' ] = $class_id = $_REQUEST[ 'class_id' ];
 }
 
-$style = file_get_contents('js/balloonscript.html');
+$style = file_get_contents( 'js/balloonscript.html' );
 
-pagehead2('pupil photos', $style);
+pagehead2( 'pupil photos', $style );
 
-$classSelectorClass = new ClassSelectorClass($dbConn, $class_id);
-$oldClassSelector = $classSelectorClass->setAutoSubmit(true)->addConstraint('sort1 < 10 and student_count <>0')->getSelector();
-
+$classSelectorClass = new ClassSelectorClass( $dbConn, $class_id );
+$oldClassSelector = $classSelectorClass->setAutoSubmit( true )->addConstraint( 'sort1 < 10 and student_count <>0' )->getSelector();
 
 $page_opening = "Pupil  photos for {$slb_info}";
-$nav = new Navigation($tutor_navtable, basename(__FILE__), $page_opening);
-$nav->setInterestMap($tabInterestCount);
+$nav = new Navigation( $tutor_navtable, basename( __FILE__ ), $page_opening );
+$nav->setInterestMap( $tabInterestCount );
 $sql = "SELECT distinct st.snummer as number," .
         "st.roepnaam||' '||coalesce(regexp_replace(st.tussenvoegsel,'''','&rsquo;')||' ','')||st.achternaam as name,\n" .
         "st.achternaam,st.roepnaam,st.tussenvoegsel,cohort,cohort,st.opl as opl_code,pcn,lang,sex,gebdat,\n" .
@@ -54,22 +54,23 @@ $sql = "SELECT distinct st.snummer as number," .
         " from student_email st \n" .
         "left join fontys_course fc on(st.opl=fc.course)\n" .
         "left join tutor_join_student td on (st.slb=td.snummer)\n" .
-        "where slb=$slb and active " .
+        "where slb=? and active " .
         "order by cohort desc,achternaam,roepnaam";
 
 //$dbConn->log($sql);
-$resultSet = $dbConn->Execute($sql);
-if ($resultSet === false) {
-    die("<br>Cannot get student_email data with \"" . $sql . '", cause ' . $dbConn->ErrorMsg() . "<br>");
+$pstm = $dbConn->prepare( $sql );
+$resultSet = $pstm->execute( [$slb ]);
+if ( $resultSet === false ) {
+    die( "<br>Cannot get student_email data with \"" . $sql . '", cause ' . $dbConn->ErrorMsg() . "<br>" );
 }
 $sql_slb = "select mine,namegrp,name,userid as value from tutor_selector($peer_id) \n"
         . "order by mine,namegrp,name";
-$slbList = "<select name='slb'>\n" . getOptionListGrouped($dbConn, $sql_slb, $slb) . "\n</select>";
+$slbList = "<select name='slb'>\n" . getOptionListGrouped( $dbConn, $sql_slb, $slb ) . "\n</select>";
 ?>
 <?= $nav->show() ?>
 <div id='navmain' style='padding:1em;'>
     <div class='nav'>
-        <form method="get" name="slb" action="<?= basename(__FILE__); ?>">
+        <form method="get" name="slb" action="<?= basename( __FILE__ ); ?>">
             <?= $slbList ?>
             <input type='submit' name='b' value ='Get fotos'/>
         </form>
@@ -77,22 +78,22 @@ $slbList = "<select name='slb'>\n" . getOptionListGrouped($dbConn, $sql_slb, $sl
     <?php
     $colcount = 0;
     $rowcount = 0;
-    $browserIE = strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') ? true : false;
+    $browserIE = strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'MSIE' ) ? true : false;
     $oldCohort = 0;
-    while (!$resultSet->EOF) {
-        extract($resultSet->fields);
-        if ($oldCohort == 0) {
+    while ( ($row = $pstm->fetch()) !== false ) {
+        extract( $row );
+        if ( $oldCohort == 0 ) {
             $oldCohort = $cohort;
         }
-        if ($cohort !== $oldCohort) {
-            if ($colcount != 0)
+        if ( $cohort !== $oldCohort ) {
+            if ( $colcount != 0 )
                 echo "</tr>\n";
-            if ($rowcount != 0)
+            if ( $rowcount != 0 )
                 echo "</table>\n";
             $rowcount = 0;
             $colcount = 0;
         }
-        if ($rowcount == 0 && $colcount == 0) {
+        if ( $rowcount == 0 && $colcount == 0 ) {
             $tablehead = "<h2>SLB Pupil photos for {$slb_info} $cohort</h2>";
             echo "{$tablehead}\n"
             . "<table><colgroup>\n"
@@ -103,11 +104,11 @@ $slbList = "<select name='slb'>\n" . getOptionListGrouped($dbConn, $sql_slb, $sl
             . "<col width='140px'/>\n"
             . "</colgroup>\n";
         }
-        if ($colcount == 0) {
+        if ( $colcount == 0 ) {
             echo "<tr>\n";
         }
 
-        if (file_exists('fotos/' . $number . '.jpg')) {
+        if ( file_exists( 'fotos/' . $number . '.jpg' ) ) {
             $photo = 'fotos/' . $number . '.jpg';
         } else {
             $photo = 'fotos/0.jpg';
@@ -126,22 +127,21 @@ $slbList = "<select name='slb'>\n" . getOptionListGrouped($dbConn, $sql_slb, $sl
         "\n</th>\n";
 
         $colcount++;
-        if ($colcount >= MAXCOL) {
+        if ( $colcount >= MAXCOL ) {
             echo "</tr>\n";
             $colcount = 0;
             $rowcount++;
-            if ($rowcount >= MAXROW) {
+            if ( $rowcount >= MAXROW ) {
                 echo "</table>\n<p style='page-break-before: always;'><!--(continued)--></p>\n";
                 $rowcount = 0;
             }
         }
 
         $oldCohort = $cohort;
-        $resultSet->moveNext();
     }
-    if ($colcount != 0)
+    if ( $colcount != 0 )
         echo "</tr>\n";
-    if ($rowcount != 0)
+    if ( $rowcount != 0 )
         echo "</table>\n";
     ?>
 </div>
