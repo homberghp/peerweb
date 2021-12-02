a74d707d70e304633fd10e2c1701aaa948f95672
Pieter van den Hombergh (homberghp) <p.vandenhombergh@fontys.nl>
2021-12-02 09:24:00 +0100

    intermediate

--- peer/maillists.inc.php
+++ peer/maillists.inc.php
@@ -1,75 +1,70 @@
 <?php
-requireCap(CAP_TUTOR);
+
+requireCap( CAP_TUTOR );
+
 /**
  * fill a mail list with the apropriate name. 
  * a cronjob will pick up this list into the email aliases every two minutes.
  * @author hom
  */
-function createMaillists($dbConn, $prjm_id) {
+function createMaillists( $dbConn, $prjm_id ) {
     $sql1 = "select lower(trim(afko)) as afko,lower(trim(course_short)) as opl,year \n" .
             " from project join prj_milestone using(prj_id) natural join fontys_course where prjm_id=$prjm_id";
-    $resultSet = $dbConn->Execute($sql1);
-    if ($resultSet === false) {
+    $resultSet = $dbConn->query( $sql1 );
+    if ( $resultSet === false ) {
         return;
     } else {
-        extract($resultSet->fields);
+        extract( $resultSet->fetch() );
         $mailalias = $opl . '.' . $afko . '.' . $year;
         $maillist_filename = '/home/maillists/' . $mailalias . '.maillist';
         $sql = "select distinct rtrim(email1) email,achternaam\n" .
                 " from project_grp_stakeholders join prj_tutor using(prjtg_id) "
                 . "join student_email using(snummer) where prjm_id=$prjm_id\n" .
                 " order by achternaam\n";
-        $resultSet = $dbConn->Execute($sql);
-        if ($resultSet === false) {
+        $resultSet = $dbConn->query( $sql );
+        if ( $resultSet === false ) {
             return;
         } else {
-
-            $handle = fopen("$maillist_filename", "w");
-            while (!$resultSet->EOF) {
-                fwrite($handle, $resultSet->fields['email'] . "\n");
-                $resultSet->moveNext();
+            $handle = fopen( "$maillist_filename", "w" );
+            while ( ($row = $resultSet->fetch()) !== false ) {
+                fwrite( $handle, $row[ 'email' ] . "\n" );
             }
-            fclose($handle);
-            //      @system('/bin/kickaliasappender');
-            $dbConn->log("created maillist with address $mailalias@fontysvenlo.org\n");
+            fclose( $handle );
         }
     }
 }
 
-function createGroupMaillists($dbConn, $prjm_id) {
+function createGroupMaillists( $dbConn, $prjm_id ) {
     $sql1 = "select maillist, email1,achternaam,roepnaam from prj_grp_email where prjm_id=$prjm_id \n"
             . "union \n"
             . "select maillist, email1,achternaam,roepnaam from prj_tutor_email where prjm_id=$prjm_id order by maillist,achternaam,roepnaam";
     ;
-    $resultSet = $dbConn->Execute($sql1);
+    $resultSet = $dbConn->query( $sql1 );
     $currentMaillist = '';
     $isOpen = false;
     $handle = 0;
 
     $lists = '';
-    if ($resultSet === false) {
+    if ( $resultSet === false ) {
         return;
     } else {
-        while (!$resultSet->EOF) {
+        while ( ($row = $resultSet->fetch()) !== false ) {
 
-            if ($currentMaillist != $resultSet->fields['maillist']) {
-                if ($isOpen) {
-                    fclose($handle);
+            if ( $currentMaillist != $row[ 'maillist' ] ) {
+                if ( $isOpen ) {
+                    fclose( $handle );
                 }
-                $currentMaillist = $resultSet->fields['maillist'];
+                $currentMaillist = $row[ 'maillist' ];
                 $lists .= $currentMaillist . "@fontysvenlo.org<br/>";
                 $maillist_filename = '/home/maillists/' . $currentMaillist . '.maillist';
-                $handle = fopen("$maillist_filename", "w");
+                $handle = fopen( "$maillist_filename", "w" );
                 $isOpen = true;
             }
-            fwrite($handle, $resultSet->fields['email1'] . "\n");
-            $resultSet->moveNext();
+            fwrite( $handle, $row[ 'email1' ] . "\n" );
         }
-        if ($isOpen) {
-            fclose($handle);
+        if ( $isOpen ) {
+            fclose( $handle );
         }
-        //@system('/bin/kickaliasappender');
-        $dbConn->log("created maillists with addresses $lists\n");
     }
 }
 
@@ -80,27 +75,24 @@ function createGroupMaillists($dbConn, $prjm_id) {
  * @param type $query
  * @return type
  */
-function createGenericMaillist($dbConn, $prefix, $query) {
-    $resultSet = $dbConn->Execute($query);
+function createGenericMaillist( $dbConn, $prefix, $query ) {
+    $resultSet = $dbConn->query( $query );
     $maillist_filename = '/home/maillists/' . $prefix . '.maillist';
-    $count=0;
-    if ($resultSet === false) {
+    $count = 0;
+    if ( $resultSet === false ) {
         return;
     } else {
-        $handle = fopen("$maillist_filename", "w");
-        while (!$resultSet->EOF) {
-            fwrite($handle, $resultSet->fields['email'] . "\n");
+        $handle = fopen( "$maillist_filename", "w" );
+        while ( ($row = $resultSet->fetch()) !== false ) {
+            fwrite( $handle, $row[ 'email' ] . "\n" );
             $count++;
-            $resultSet->moveNext();
         }
-        fclose($handle);
+        fclose( $handle );
         //@system('/bin/kickaliasappender');
-        if ($count > 0) {
-            chmod($maillist_filename, 0664);
-            $dbConn->log("created maillist with address $prefix@fontysvenlo.org\n");
+        if ( $count > 0 ) {
+            chmod( $maillist_filename, 0664 );
         } else {
-            unlink($maillist_filename);
-            $dbConn->log("removed empty maillist with address $prefix@fontysvenlo.org\n");        
+            unlink( $maillist_filename );
         }
     }
 }
@@ -111,15 +103,15 @@ function createGenericMaillist($dbConn, $prefix, $query) {
  * @param type $prefix in front of @fontysvenlo.org
  * @param type $class_id class
  */
-function createGenericMaillistByClassid($dbConn, $class_id) {
-    $prefix ='noprefix';
-    $sql ="select lower(rtrim(faculty_short)||'.'||rtrim(sclass)) as prefix from student_class join faculty using(faculty_id) where class_id={$class_id}";
-    $resultSet = $dbConn->Execute($sql);
-    if ($resultSet === false) {
+function createGenericMaillistByClassid( $dbConn, $class_id ) {
+    $prefix = 'noprefix';
+    $sql = "select lower(rtrim(faculty_short)||'.'||rtrim(sclass)) as prefix from student_class join faculty using(faculty_id) where class_id={$class_id}";
+    $resultSet = $dbConn->query( $sql );
+    if ( $resultSet === false ) {
         echo "$sql";
         return;
     } else {
-        $prefix=$resultSet->fields['prefix'];
+        $prefix = $resultSet->fetch()[ 'prefix' ];
     }
-    createGenericMaillist($dbConn,$prefix,"select email1 as email from student_email where class_id=$class_id");
+    createGenericMaillist( $dbConn, $prefix, "select email1 as email from student_email where class_id=$class_id" );
 }
